3df44ed2d85924262a8504b39ca0fdc0ff2e1162
rajuX75 <rajux75@example.com>
2025-01-11 13:47:28 +0600

    Unknown

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,7 +2,6 @@ import { db } from "@/server/db";
 
 export const POST = async (req: Request) => {
   const { data } = await req.json();
-  console.log("clerk webhook received");
   const emailAddress = data.email_addresses[0].email_address;
   const firstName = data.first_name;
   const lastName = data.last_name;
@@ -12,12 +11,11 @@ export const POST = async (req: Request) => {
   await db.user.create({
     data: {
       id: id,
-      emailAddress: emailAddress,
+      email: emailAddress,
       firstName: firstName,
       lastName: lastName,
       imageUrl: imageUrl,
     },
   });
-  console.log("Created user");
   return new Response("Webhook received", { status: 200 });
 };
